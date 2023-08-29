f24611dbe2023178828ce88504cd615adb90bec8
Bakhtiyorovna <kralecabaxtiyorovna@gmail.com>
2023-08-29 10:26:37 +0500

    The DataAccess part has been written

---
+++ src/FileSystem.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using FileSystem.Domain.Entities;
+using FileSystem.Domain.Entities.Files;
+
+namespace FileSystem.DataAccess.Interfaces;
+
+public interface IRepository
+{
+    public Task<int> IFileSaveAsync(FileEntity file);
+}
